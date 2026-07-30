4dd26bef0b1540730cfb3a123c854981d2f92e22
Meet-08 <meetjbhuva@gmail.com>
2026-07-30 21:01:00 +0530

    fix: mapping issue

--- server/src/main/java/com/meet/server/feature/codechunk/CodeChunk.java
+++ server/src/main/java/com/meet/server/feature/codechunk/CodeChunk.java
@@ -35,14 +35,11 @@ public class CodeChunk extends BaseAuditEntity {
     private UUID id;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumns({
-            @JoinColumn(name = "file_id", referencedColumnName = "id", nullable = false),
-            @JoinColumn(name = "codebase_id", referencedColumnName = "codebase_id", nullable = false)
-    })
+    @JoinColumn(name = "file_id", nullable = false)
     private RepositoryFile file;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "codebase_id", nullable = false, insertable = false, updatable = false)
+    @JoinColumn(name = "codebase_id", nullable = false)
     private Codebase codebase;
 
     @Column(name = "chunk_index", nullable = false)
